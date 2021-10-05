0b380329a6e0c04243cc7595f10e4ff9adf152af
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2021-10-05 15:55:08 +0200

    Add Swagger UI and expose openapi spec
    Co-authored-by: Kyrre Havik <kyrremann@gmail.com>
    Co-authored-by: vebjorre <vebjorn.rekkebo@nav.no>

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"embed"
+	"encoding/json"
+	"io/fs"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -11,9 +14,12 @@ import (
 	"golang.org/x/oauth2"
 )
 
+//go:embed swagger/*
+var swagger embed.FS
+
 func NewRouter(repo *database.Repo, oauth2Config oauth2.Config, log *logrus.Entry, middlewares ...openapi.MiddlewareFunc) http.Handler {
 	corsMW := cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowCredentials: true,
 	})
@@ -26,6 +32,21 @@ func NewRouter(repo *database.Repo, oauth2Config oauth2.Config, log *logrus.Entr
 	baseRouter.Get("/api/oauth2/callback", srv.Callback)
 	baseRouter.Get("/internal/isalive", func(rw http.ResponseWriter, r *http.Request) {})
 	baseRouter.Get("/internal/isready", func(rw http.ResponseWriter, r *http.Request) {})
+	baseRouter.Get("/api/spec", func(rw http.ResponseWriter, r *http.Request) {
+		spec, err := openapi.GetSwagger()
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_ = json.NewEncoder(rw).Encode(spec)
+	})
+
+	swaggerFS, err := fs.Sub(swagger, "swagger")
+	if err != nil {
+		panic(err)
+	}
+	baseRouter.Handle("/api/*", http.StripPrefix("/api/", http.FileServer(http.FS(swaggerFS))))
 
 	router := openapi.HandlerWithOptions(srv, openapi.ChiServerOptions{BaseRouter: baseRouter, BaseURL: "/api", Middlewares: middlewares})
 	return router
