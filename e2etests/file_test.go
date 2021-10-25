299b9cdf35ac49c1830e7cdc9140711b5cf1b77d
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2021-10-25 16:49:07 +0200

    Add tests for dataproduct access
    Change e2e test framework to allow for options to ignore or check !null
    for paths.
    
    Co-authored-by: Erik Vattekar <erik.vattekar@nav.no>

--- e2etests/file_test.go
+++ e2etests/file_test.go
@@ -5,12 +5,13 @@ package e2etests
 import (
 	"bytes"
 	"encoding/json"
+	"html/template"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
-	"text/template"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -52,7 +53,7 @@ func TestFiles(t *testing.T) {
 
 func testFile(t *testing.T, state *state, fname string) {
 	t.Run(fname, func(t *testing.T) {
-		q, expected, store, err := splitTestFile(fname)
+		q, expected, store, options, err := splitTestFile(fname, state)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -61,8 +62,23 @@ func testFile(t *testing.T, state *state, fname string) {
 			t.Fatal(err)
 		}
 
-		if !cmp.Equal(val, expected, cmp.FilterPath(ignoreID, cmp.Ignore())) {
-			t.Error(cmp.Diff(val, expected, cmp.FilterPath(ignoreID, cmp.Ignore())))
+		opts := []cmp.Option{}
+
+		for _, o := range options {
+			var of cmp.Option
+			switch {
+			case o.notnull:
+				of = cmp.Comparer(cmpNotNull)
+			case o.ignore:
+				of = cmp.Ignore()
+			default:
+				t.Fatal("unexpected option on path", o.path)
+			}
+
+			opts = append(opts, cmp.FilterPath(ignorePath(o.path), of))
+		}
+		if !cmp.Equal(val, expected, opts...) {
+			t.Error(cmp.Diff(val, expected, opts...))
 		}
 	})
 }
@@ -72,18 +88,52 @@ type storeRequest struct {
 	path string
 }
 
-func splitTestFile(fname string) (q string, expected map[string]interface{}, store []storeRequest, err error) {
+type cmpOpt struct {
+	path    string
+	notnull bool
+	ignore  bool
+}
+
+func splitTestFile(fname string, state *state) (q string, expected map[string]interface{}, store []storeRequest, options []cmpOpt, err error) {
 	b, err := ioutil.ReadFile("testdata/" + fname)
 	if err != nil {
-		return "", nil, nil, err
+		return "", nil, nil, nil, err
 	}
 
-	parts := bytes.SplitN(b, []byte("RETURNS"), 2)
+	tpl, err := template.New("query").Parse(string(b))
+	if err != nil {
+		return "", nil, nil, nil, err
+	}
+
+	buf := &bytes.Buffer{}
+	if err := tpl.Execute(buf, state.data); err != nil {
+		return "", nil, nil, nil, err
+	}
+
+	parts := bytes.SplitN(buf.Bytes(), []byte("RETURNS"), 2)
 	q = string(parts[0])
 
-	srs := bytes.Split(parts[1], []byte("STORE"))
+	optParts := bytes.SplitN(parts[1], []byte("ENDOPTS"), 2)
+	returns := optParts[len(optParts)-1]
+
+	if len(optParts) > 1 {
+		os := strings.Split(string(optParts[0]), "OPTION")
+		for _, o := range os {
+			if strings.TrimSpace(o) == "" {
+				continue
+			}
+			ps := strings.SplitN(o, "=", 2)
+			options = append(options, cmpOpt{
+				path:    strings.TrimSpace(ps[0]),
+				notnull: strings.TrimSpace(ps[1]) == "NOTNULL",
+				ignore:  strings.TrimSpace(ps[1]) == "IGNORE",
+			})
+		}
+	}
+
+	srs := bytes.Split(returns, []byte("STORE"))
 	if err := json.Unmarshal(srs[0], &expected); err != nil {
-		return "", nil, nil, err
+		return "", nil, nil, nil, err
 	}
 
 	if len(srs) > 1 {
@@ -96,11 +146,33 @@ func splitTestFile(fname string) (q string, expected map[string]interface{}, sto
 		}
 	}
 
-	return q, expected, store, nil
+	return q, expected, store, options, nil
 }
 
 func ignoreID(p cmp.Path) bool {
 	return p.Last().String() == `["id"]`
+}
+
+func ignorePath(path string) func(p cmp.Path) bool {
+	return func(p cmp.Path) bool {
+		s := ""
+		for _, pe := range p {
+			switch pe := pe.(type) {
+			case cmp.MapIndex:
+				s += "." + pe.Key().String()
+			case cmp.SliceIndex:
+				s += "." + strconv.Itoa(pe.Key())
+			}
+		}
+		return s == "."+path
+	}
+}
+
+func cmpNotNull(a, b interface{}) bool {
+	if a == nil || b == nil {
+		return false
+	}
+	return true
 }
 
 type state struct {
@@ -108,16 +180,6 @@ type state struct {
 }
 
 func doQuery(state *state, q string, store []storeRequest) (map[string]interface{}, error) {
-	tpl, err := template.New("query").Parse(q)
-	if err != nil {
-		return nil, err
-	}
-
-	buf := &bytes.Buffer{}
-	if err := tpl.Execute(buf, state.data); err != nil {
-		return nil, err
-	}
-
 	b, err := json.Marshal(
 		struct {
 			OperationName *string                `json:"operationName"`
@@ -126,7 +188,7 @@ func doQuery(state *state, q string, store []storeRequest) (map[string]interface
 		}{
 			nil,
 			map[string]interface{}{},
-			buf.String(),
+			q,
 		},
 	)
 	if err != nil {
@@ -169,4 +231,20 @@ func doQuery(state *state, q string, store []storeRequest) (map[string]interface
 	}
 
 	return ret, nil
+}
+
+func customcmp(opts ...cmp.Option) func(a, b interface{}) bool {
+	return func(a, b interface{}) bool {
+		as, ok := a.(string)
+		if ok && as == "!NOTNULL" {
+			return b != nil
+		}
+
+		bs, ok := a.(string)
+		if ok && bs == "!NOTNULL" {
+			return a != nil
+		}
+
+		return cmp.Equal(a, b)
+	}
 }
