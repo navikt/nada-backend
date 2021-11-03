999e28513799d125d2b138631f8ec1b63d60b914
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2021-11-03 14:40:54 +0100

    Use  as FragmentDelimiter in search result

--- pkg/database/gensql/search.sql.go
+++ pkg/database/gensql/search.sql.go
@@ -14,7 +14,7 @@ SELECT
 	element_id::uuid,
 	element_type::text,
 	ts_rank_cd(tsv_document, query) AS rank,
-	ts_headline('norwegian', "description", query, 'MinWords=14, MaxWords=15, MaxFragments=2')::text AS excerpt
+	ts_headline('norwegian', "description", query, 'MinWords=14, MaxWords=15, MaxFragments=2 FragmentDelimiter=" … "')::text AS excerpt
 FROM
 	search,
 	websearch_to_tsquery('norwegian', $1) query
