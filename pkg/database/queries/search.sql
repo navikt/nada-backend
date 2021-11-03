f9bfad4406a8c679e7287c1ae8201dd4da71b625
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2021-11-03 11:42:15 +0100

    Make excerpt using norwegian ts

--- pkg/database/queries/search.sql
+++ pkg/database/queries/search.sql
@@ -3,7 +3,7 @@ SELECT
 	element_id::uuid,
 	element_type::text,
 	ts_rank_cd(tsv_document, query) AS rank,
-	ts_headline("description", query, 'MinWords=14, MaxWords=15, MaxFragments=2')::text AS excerpt
+	ts_headline('norwegian', "description", query, 'MinWords=14, MaxWords=15, MaxFragments=2')::text AS excerpt
 FROM
 	search,
 	websearch_to_tsquery('norwegian', @query) query
