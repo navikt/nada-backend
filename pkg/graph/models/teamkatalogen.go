6aa4ad9f580ce40d4a16eb127b994669359e361f
Kent D <kent.daleng@nav.no>
2021-11-22 09:54:22 +0100

    fetch all teams and filter before sending result

--- pkg/graph/models/teamkatalogen.go
+++ pkg/graph/models/teamkatalogen.go
@@ -1,7 +1,9 @@
 package models
 
 type TeamkatalogenResult struct {
-	URL         string `json:"url"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	URL         string   `json:"url"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	NaisTeams   []string `json:"naisTeams"`
+	Tags        []string `json:"tags"`
 }
