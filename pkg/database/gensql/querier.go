a7e5f10a57e43f259142dd25575cf13ab602962b
frodesundby <frode.sundby@nav.no>
2021-12-07 10:10:02 +0100

    deploy to dev-gcp

--- pkg/database/gensql/querier.go
+++ pkg/database/gensql/querier.go
@@ -10,24 +10,14 @@ import (
 
 type Querier interface {
 	CreateBigqueryDatasource(ctx context.Context, arg CreateBigqueryDatasourceParams) (DatasourceBigquery, error)
-	CreateCollection(ctx context.Context, arg CreateCollectionParams) (Collection, error)
-	CreateCollectionElement(ctx context.Context, arg CreateCollectionElementParams) error
 	CreateDataproduct(ctx context.Context, arg CreateDataproductParams) (Dataproduct, error)
 	CreateDataproductRequester(ctx context.Context, arg CreateDataproductRequesterParams) error
 	CreateMetabaseMetadata(ctx context.Context, arg CreateMetabaseMetadataParams) error
-	DeleteCollection(ctx context.Context, id uuid.UUID) error
-	DeleteCollectionElement(ctx context.Context, arg DeleteCollectionElementParams) error
 	DeleteDataproduct(ctx context.Context, id uuid.UUID) error
 	DeleteDataproductRequester(ctx context.Context, arg DeleteDataproductRequesterParams) error
 	GetAccessToDataproduct(ctx context.Context, id uuid.UUID) (DataproductAccess, error)
 	GetBigqueryDatasource(ctx context.Context, dataproductID uuid.UUID) (DatasourceBigquery, error)
 	GetBigqueryDatasources(ctx context.Context) ([]DatasourceBigquery, error)
-	GetCollection(ctx context.Context, id uuid.UUID) (Collection, error)
-	GetCollectionElements(ctx context.Context, collectionID uuid.UUID) ([]Dataproduct, error)
-	GetCollections(ctx context.Context, arg GetCollectionsParams) ([]Collection, error)
-	GetCollectionsByGroups(ctx context.Context, groups []string) ([]Collection, error)
-	GetCollectionsByIDs(ctx context.Context, ids []uuid.UUID) ([]Collection, error)
-	GetCollectionsForElement(ctx context.Context, arg GetCollectionsForElementParams) ([]Collection, error)
 	GetDataproduct(ctx context.Context, id uuid.UUID) (Dataproduct, error)
 	GetDataproductMappings(ctx context.Context, dataproductID uuid.UUID) (ThirdPartyMapping, error)
 	GetDataproductRequesters(ctx context.Context, dataproductID uuid.UUID) ([]string, error)
@@ -45,7 +35,6 @@ type Querier interface {
 	RevokeAccessToDataproduct(ctx context.Context, id uuid.UUID) error
 	Search(ctx context.Context, arg SearchParams) ([]SearchRow, error)
 	UpdateBigqueryDatasourceSchema(ctx context.Context, arg UpdateBigqueryDatasourceSchemaParams) error
-	UpdateCollection(ctx context.Context, arg UpdateCollectionParams) (Collection, error)
 	UpdateDataproduct(ctx context.Context, arg UpdateDataproductParams) (Dataproduct, error)
 }
 
--- pkg/graph/dataproducts.resolvers.go
+++ pkg/graph/dataproducts.resolvers.go
@@ -227,7 +227,5 @@ func (r *Resolver) BigQuery() generated.BigQueryResolver { return &bigQueryResol
 // Dataproduct returns generated.DataproductResolver implementation.
 func (r *Resolver) Dataproduct() generated.DataproductResolver { return &dataproductResolver{r} }
 
-type (
-	bigQueryResolver    struct{ *Resolver }
-	dataproductResolver struct{ *Resolver }
-)
+type bigQueryResolver struct{ *Resolver }
+type dataproductResolver struct{ *Resolver }
