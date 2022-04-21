145d98e6f9fd334cd41f3cea8de211707a9305c7
Erik Vattekar <erik.vattekar@nav.no>
2022-04-21 13:36:35 +0200

    return null when there is no polly documentation connected to the access grant

--- pkg/database/polly.go
+++ pkg/database/polly.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 
 	"github.com/google/uuid"
 	"github.com/navikt/nada-backend/pkg/graph/models"
@@ -10,6 +12,9 @@ import (
 func (r *Repo) GetAccessDocumentation(ctx context.Context, accessID uuid.UUID) (*models.Polly, error) {
 	pollySQL, err := r.querier.GetAccessDocumentation(ctx, accessID)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
 		return nil, err
 	}
 
