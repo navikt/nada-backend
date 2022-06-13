5ba97a4c1849917651107a4a6f55774965428d60
Erik Vattekar <erik.vattekar@nav.no>
2022-06-13 08:48:38 +0200

    rename triggers

--- pkg/database/migrations/0052_update_dataproduct_trigger.sql
+++ pkg/database/migrations/0052_update_dataproduct_trigger.sql
@@ -1,20 +1,21 @@
 -- +goose Up
 DROP TRIGGER datasource_bigquery_set_modified ON datasource_bigquery;
 
-CREATE TRIGGER dataproducts_set_modified
+CREATE TRIGGER dataproducts_datasets_set_modified
     BEFORE UPDATE
     ON datasets
     FOR EACH ROW
 EXECUTE PROCEDURE update_dataproduct_modified_timestamp();
 
-CREATE TRIGGER datasets_set_modified
+CREATE TRIGGER datasets_datasource_bigquery_set_modified
     BEFORE UPDATE
     ON datasource_bigquery
     FOR EACH ROW
 EXECUTE PROCEDURE update_dataset_modified_timestamp();
 
 -- +goose Down
-DROP TRIGGER dataproducts_set_modified ON datasets;
+DROP TRIGGER dataproducts_datasets_set_modified ON datasets;
+DROP TRIGGER datasets_datasource_bigquery_set_modified ON datasets;
 
 CREATE TRIGGER datasource_bigquery_set_modified
     BEFORE UPDATE
