555826f70fa6ae0942e7f38533d963452476f84c
Erik Vattekar <erik.vattekar@nav.no>
2022-08-02 13:24:03 +0200

    temp test with lower team projects update interval

--- cmd/nada-backend/main.go
+++ cmd/nada-backend/main.go
@@ -41,7 +41,7 @@ var (
 )
 
 const (
-	TeamProjectsUpdateFrequency = 60 * time.Minute
+	TeamProjectsUpdateFrequency = 5 * time.Minute
 	AccessEnsurerFrequency      = 5 * time.Minute
 	MetabaseUpdateFrequency     = 5 * time.Minute
 	StoryDraftCleanerFrequency  = 24 * time.Hour
