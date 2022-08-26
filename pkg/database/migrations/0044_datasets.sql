92b7e390f78689ada4171e2eafbf782fa94ae8d1
Kent D <kent.daleng@nav.no>
2022-08-26 12:11:17 +0200

    fix ambiguities

--- pkg/database/migrations/0044_datasets.sql
+++ pkg/database/migrations/0044_datasets.sql
@@ -76,7 +76,7 @@ ALTER TABLE dataproduct_requesters
     DROP CONSTRAINT fk_requester_dataset;
 ALTER TABLE dataproduct_requesters
     ADD COLUMN dataproduct_id uuid;
-UPDATE dataproduct_requesters a SET dataproduct_id = (SELECT dataproduct_id FROM datasets WHERE dataset_id = a.dataset_id);
+UPDATE dataproduct_requesters a SET dataproduct_id = (SELECT dataproduct_id FROM datasets e WHERE e.id = a.dataset_id);
 ALTER TABLE dataproduct_requesters
     ALTER COLUMN dataproduct_id SET NOT NULL;
 ALTER TABLE dataproduct_requesters DROP CONSTRAINT dataset_requesters_pkey;
@@ -89,11 +89,11 @@ ALTER TABLE dataproduct_requesters
         REFERENCES dataproducts (id) ON DELETE CASCADE;
 
 ALTER TABLE datasource_bigquery
-    DROP CONSTRAINT fk_bigquery_dataset;
-UPDATE datasource_bigquery a SET dataset_id = (SELECT dataproduct_id FROM datasets WHERE dataset_id = a.dataset_id);
-
-ALTER TABLE datasource_bigquery
     RENAME COLUMN dataset_id TO dataproduct_id;
+ALTER TABLE datasource_bigquery
+    DROP CONSTRAINT fk_bigquery_dataset;
+UPDATE datasource_bigquery a SET dataproduct_id = (SELECT dataproduct_id FROM datasets e WHERE e.id = a.dataproduct_id);
+
 
 ALTER TABLE datasource_bigquery
     ADD CONSTRAINT fk_bigquery_dataproduct FOREIGN KEY (dataproduct_id)
