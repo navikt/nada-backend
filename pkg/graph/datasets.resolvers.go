143e5536dd0543ddd116e508ee7fe16a354822f6
Kent D <kent.daleng@nav.no>
2022-11-08 11:03:42 +0100

    fix datasets resolvers

--- pkg/graph/datasets.resolvers.go
+++ pkg/graph/datasets.resolvers.go
@@ -5,10 +5,15 @@ package graph
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
 	"html"
+	"os"
+	"strings"
 
 	"github.com/google/uuid"
+	"github.com/navikt/nada-backend/pkg/auth"
 	"github.com/navikt/nada-backend/pkg/graph/generated"
 	"github.com/navikt/nada-backend/pkg/graph/models"
 )
@@ -38,67 +43,239 @@ func (r *datasetResolver) Description(ctx context.Context, obj *models.Dataset, 
 
 // Owner is the resolver for the owner field.
 func (r *datasetResolver) Owner(ctx context.Context, obj *models.Dataset) (*models.Owner, error) {
-	panic(fmt.Errorf("not implemented: Owner - owner"))
+	dp, err := r.repo.GetDataproduct(ctx, obj.DataproductID)
+	if err != nil {
+		return nil, err
+	}
+	return dp.Owner, nil
 }
 
 // Datasource is the resolver for the datasource field.
 func (r *datasetResolver) Datasource(ctx context.Context, obj *models.Dataset) (models.Datasource, error) {
-	panic(fmt.Errorf("not implemented: Datasource - datasource"))
+	return r.repo.GetBigqueryDatasource(ctx, obj.ID)
 }
 
 // Access is the resolver for the access field.
 func (r *datasetResolver) Access(ctx context.Context, obj *models.Dataset) ([]*models.Access, error) {
-	panic(fmt.Errorf("not implemented: Access - access"))
+	all, err := r.repo.ListAccessToDataset(ctx, obj.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	dp, err := r.repo.GetDataproduct(ctx, obj.DataproductID)
+	if err != nil {
+		return nil, err
+	}
+
+	var ret []*models.Access
+
+	user := auth.GetUser(ctx)
+	if user == nil {
+		return ret, nil
+	}
+	if user.GoogleGroups.Contains(dp.Owner.Group) {
+		return all, nil
+	}
+
+	for _, a := range all {
+		if strings.EqualFold(a.Subject, "user:"+user.Email) {
+			ret = append(ret, a)
+		} else if strings.HasPrefix(a.Subject, "group:") && user.GoogleGroups.Contains(strings.TrimPrefix(a.Subject, "group:")) {
+			ret = append(ret, a)
+		}
+	}
+
+	return ret, nil
 }
 
 // Services is the resolver for the services field.
 func (r *datasetResolver) Services(ctx context.Context, obj *models.Dataset) (*models.DatasetServices, error) {
-	panic(fmt.Errorf("not implemented: Services - services"))
+	meta, err := r.repo.GetMetabaseMetadata(ctx, obj.ID, false)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return &models.DatasetServices{}, nil
+		}
+		return nil, err
+	}
+
+	svc := &models.DatasetServices{}
+	if meta.DatabaseID > 0 {
+		base := "https://metabase.dev.intern.nav.no/browse/%v"
+		if os.Getenv("NAIS_CLUSTER_NAME") == "prod-gcp" {
+			base = "https://metabase.intern.nav.no/browse/%v"
+		}
+		url := fmt.Sprintf(base, meta.DatabaseID)
+		svc.Metabase = &url
+	}
+
+	return svc, nil
 }
 
 // Mappings is the resolver for the mappings field.
 func (r *datasetResolver) Mappings(ctx context.Context, obj *models.Dataset) ([]models.MappingService, error) {
-	panic(fmt.Errorf("not implemented: Mappings - mappings"))
+	return r.repo.GetDataproductMappings(ctx, obj.ID)
 }
 
 // Requesters is the resolver for the requesters field.
 func (r *datasetResolver) Requesters(ctx context.Context, obj *models.Dataset) ([]string, error) {
-	panic(fmt.Errorf("not implemented: Requesters - requesters"))
+	allRequesters, err := r.repo.GetDatasetRequesters(ctx, obj.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	dp, err := r.repo.GetDataproduct(ctx, obj.DataproductID)
+	if err != nil {
+		return nil, err
+	}
+
+	user := auth.GetUser(ctx)
+	if user.GoogleGroups.Contains(dp.Owner.Group) {
+		return allRequesters, nil
+	}
+
+	var ret []string
+	for _, r := range allRequesters {
+		if strings.EqualFold(r, user.Email) {
+			ret = append(ret, r)
+		} else if user.GoogleGroups.Contains(r) {
+			ret = append(ret, r)
+		}
+	}
+
+	return ret, nil
 }
 
 // CreateDataset is the resolver for the createDataset field.
 func (r *mutationResolver) CreateDataset(ctx context.Context, input models.NewDataset) (*models.Dataset, error) {
-	panic(fmt.Errorf("not implemented: CreateDataset - createDataset"))
+	dp, err := r.repo.GetDataproduct(ctx, input.DataproductID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ensureUserInGroup(ctx, dp.Owner.Group); err != nil {
+		return nil, err
+	}
+
+	metadata, err := r.prepareBigQuery(ctx, input.BigQuery, dp.Owner.Group)
+	if err != nil {
+		return nil, err
+	}
+
+	input.Metadata = metadata
+	if input.Description != nil && *input.Description != "" {
+		*input.Description = html.EscapeString(*input.Description)
+	}
+	ds, err := r.repo.CreateDataset(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+	return ds, nil
 }
 
 // UpdateDataset is the resolver for the updateDataset field.
 func (r *mutationResolver) UpdateDataset(ctx context.Context, id uuid.UUID, input models.UpdateDataset) (*models.Dataset, error) {
-	panic(fmt.Errorf("not implemented: UpdateDataset - updateDataset"))
+	ds, err := r.repo.GetDataset(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	dp, err := r.repo.GetDataproduct(ctx, ds.DataproductID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ensureUserInGroup(ctx, dp.Owner.Group); err != nil {
+		return nil, err
+	}
+
+	if input.DataproductID != nil && *input.DataproductID != ds.DataproductID {
+		dp2, err := r.repo.GetDataproduct(ctx, *input.DataproductID)
+		if err != nil {
+			return nil, err
+		}
+		if err := ensureUserInGroup(ctx, dp2.Owner.Group); err != nil {
+			return nil, err
+		}
+		if dp.Owner.Group != dp2.Owner.Group {
+			return nil, errors.New("cannot move dataset to a dataproduct that is not owned by the same team")
+		}
+	} else {
+		input.DataproductID = &ds.DataproductID
+	}
+
+	if input.Description != nil && *input.Description != "" {
+		*input.Description = html.EscapeString(*input.Description)
+	}
+	return r.repo.UpdateDataset(ctx, id, input)
 }
 
 // DeleteDataset is the resolver for the deleteDataset field.
 func (r *mutationResolver) DeleteDataset(ctx context.Context, id uuid.UUID) (bool, error) {
-	panic(fmt.Errorf("not implemented: DeleteDataset - deleteDataset"))
+	ds, err := r.repo.GetDataset(ctx, id)
+	if err != nil {
+		return false, err
+	}
+
+	dp, err := r.repo.GetDataproduct(ctx, ds.DataproductID)
+	if err != nil {
+		return false, err
+	}
+	if err := ensureUserInGroup(ctx, dp.Owner.Group); err != nil {
+		return false, err
+	}
+
+	return true, r.repo.DeleteDataset(ctx, ds.ID)
 }
 
 // MapDataset is the resolver for the mapDataset field.
 func (r *mutationResolver) MapDataset(ctx context.Context, datasetID uuid.UUID, services []models.MappingService) (bool, error) {
-	panic(fmt.Errorf("not implemented: MapDataset - mapDataset"))
+	ds, err := r.repo.GetDataset(ctx, datasetID)
+	if err != nil {
+		return false, err
+	}
+
+	dp, err := r.repo.GetDataproduct(ctx, ds.DataproductID)
+	if err != nil {
+		return false, err
+	}
+	if err := ensureUserInGroup(ctx, dp.Owner.Group); err != nil {
+		return false, err
+	}
+
+	err = r.repo.MapDataset(ctx, datasetID, services)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
 // Dataset is the resolver for the dataset field.
 func (r *queryResolver) Dataset(ctx context.Context, id uuid.UUID) (*models.Dataset, error) {
-	panic(fmt.Errorf("not implemented: Dataset - dataset"))
+	return r.repo.GetDataset(ctx, id)
 }
 
 // AccessRequestsForDataset is the resolver for the accessRequestsForDataset field.
 func (r *queryResolver) AccessRequestsForDataset(ctx context.Context, datasetID uuid.UUID) ([]*models.AccessRequest, error) {
-	panic(fmt.Errorf("not implemented: AccessRequestsForDataset - accessRequestsForDataset"))
+	ds, err := r.repo.GetDataset(ctx, datasetID)
+	if err != nil {
+		return nil, err
+	}
+
+	dp, err := r.repo.GetDataproduct(ctx, ds.DataproductID)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ensureUserInGroup(ctx, dp.Owner.Group); err != nil {
+		return nil, err
+	}
+
+	return r.repo.ListAccessRequestsForDataset(ctx, datasetID)
 }
 
 // DatasetsInDataproduct is the resolver for the datasetsInDataproduct field.
 func (r *queryResolver) DatasetsInDataproduct(ctx context.Context, dataproductID uuid.UUID) ([]*models.Dataset, error) {
-	panic(fmt.Errorf("not implemented: DatasetsInDataproduct - datasetsInDataproduct"))
+	return r.repo.GetDatasetsInDataproduct(ctx, dataproductID)
 }
 
 // BigQuery returns generated.BigQueryResolver implementation.
