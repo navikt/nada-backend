849f767ab11558b47031979816d7e60164175659
Meng Zhu <meng.zhu@nav.no>
2022-12-15 10:21:20 +0100

    remove mock slack new dataproduct

--- pkg/slack/mock.go
+++ pkg/slack/mock.go
@@ -15,11 +15,6 @@ func NewMockSlackClient(log *logrus.Logger) *MockSlackClient {
 	}
 }
 
-func (m MockSlackClient) NewDataproduct(dp *models.Dataproduct) error {
-	m.log.Info("NewDataProduct")
-	return nil
-}
-
 func (m MockSlackClient) NewAccessRequest(contact string, dp *models.Dataproduct, ds *models.Dataset, ar *models.AccessRequest) error {
 	m.log.Info("New AccessRequest send to " + contact)
 	return nil
