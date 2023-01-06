acbc7716fd5609d2ae74eda167c0fe1205e46958
Meng Zhu <meng.zhu@nav.no>
2023-01-06 09:50:32 +0100

    debg

--- pkg/metabase/client.go
+++ pkg/metabase/client.go
@@ -626,7 +626,7 @@ func (c *Client) grantAADGroupOwnerPermission(ctx context.Context, aadGroupID in
 	}
 
 	payload, err := json.Marshal(permissionGraph)
-	log.Printf("permission request with payload %v, error %v", payload, err)
+	log.Printf("permission request with payload %v, error %v", string(payload), err)
 	if err := c.request(ctx, http.MethodPut, "/permissions/graph", permissionGraph, nil); err != nil {
 		return err
 	}
