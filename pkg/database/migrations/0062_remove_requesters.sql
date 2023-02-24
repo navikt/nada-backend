6ffdbd9d20b429f1bb27f5b0ac8e0e84784eafd1
Erik Vattekar <erik.vattekar@nav.no>
2023-02-24 14:22:20 +0100

    fix: migration typo

--- pkg/database/migrations/0062_remove_requesters.sql
+++ pkg/database/migrations/0062_remove_requesters.sql
@@ -6,7 +6,7 @@ CREATE TABLE dataset_requesters(
     dataset_id uuid NOT NULL,
     "subject" TEXT NOT NULL,
     PRIMARY KEY (dataset_id, "subject"),
-    CONSTRAINT fk_requester_dataproduct
+    CONSTRAINT fk_requester_dataset
         FOREIGN KEY (dataset_id)
             REFERENCES datasets (id) ON DELETE CASCADE
 );
