a09cacbdf596176881194af24660a176b43d1d02
Erik Vattekar <erik.vattekar@nav.no>
2023-03-31 16:23:43 +0200

    nada-backend user does not exist for tests

--- pkg/database/migrations/0068_configure_datastream_user.sql
+++ pkg/database/migrations/0068_configure_datastream_user.sql
@@ -1,12 +1,12 @@
 -- +goose Up
-ALTER USER "nada-backend" WITH REPLICATION;
-CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
-
 -- +goose StatementBegin
 DO
 $$
     BEGIN
         IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            ALTER USER "nada-backend" WITH REPLICATION;
+            CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
+            
             ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
             GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "datastream";
             ALTER USER "datastream" WITH REPLICATION;
@@ -24,9 +24,10 @@ $$
             ALTER USER "datastream" WITH NOREPLICATION;
             REVOKE SELECT ON ALL TABLES IN SCHEMA PUBLIC FROM "datastream";
             ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC REVOKE SELECT ON TABLES FROM "datastream";
+            
+            DROP PUBLICATION "ds_publication";
+            ALTER USER "nada-backend" WITH NOREPLICATION;
         END IF;
     END
 $$ LANGUAGE 'plpgsql';
 -- +goose StatementEnd
-DROP PUBLICATION "ds_publication";
-ALTER USER "nada-backend" WITH NOREPLICATION;
--- pkg/database/migrations/0069_create_replication_slot.sql
+++ pkg/database/migrations/0069_create_replication_slot.sql
@@ -1,5 +1,23 @@
 -- +goose Up
-SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+-- +goose StatementBegin
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
+-- +goose StatementEnd
 
 -- +goose Down
-SELECT pg_drop_replication_slot('ds_replication');
+-- +goose StatementBegin
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            SELECT pg_drop_replication_slot('ds_replication');
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
+-- +goose StatementEnd
