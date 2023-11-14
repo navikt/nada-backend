86902c58d192e0b39670e8b2d872d0a375cd87b5
Meng Zhu <meng.zhu@nav.no>
2023-11-14 13:07:39 +0100

    migration

--- pkg/database/migrations/0076_trigger_for_delete_dataset.sql
+++ pkg/database/migrations/0076_trigger_for_delete_dataset.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 ALTER TABLE
-    joinable_views_datasource DROP CONSTRAINT fk_datasource_id;
+    joinable_views_datasource DROP CONSTRAINT IF EXISTS fk_datasource_id;
 
 ALTER TABLE
-    datasource_bigquery DROP CONSTRAINT fk_bigquery_dataset;
+    datasource_bigquery DROP CONSTRAINT IF EXISTS fk_bigquery_dataset;
 
 ALTER TABLE
     joinable_views_datasource
-ADD
+ADD 
     COLUMN "deleted" TIMESTAMPTZ;
 
 CREATE
@@ -17,19 +17,19 @@ LANGUAGE plpgsql;
 CREATE TRIGGER dataset_deleted BEFORE DELETE ON datasets FOR EACH ROW EXECUTE PROCEDURE set_joinable_view_deleted_on_dataset_deleted();
 
 -- +goose Down
-DROP TRIGGER joinable_view_deleted ON datasource_bigquery;
+DROP TRIGGER IF EXISTS dataset_deleted ON datasets;
 
-DROP FUNCTION set_joinable_view_deleted_on_datasource_deleted;
+DROP FUNCTION IF EXISTS set_joinable_view_deleted_on_datasource_deleted;
 
 ALTER TABLE
-    joinable_views_datasource DROP COLUMN "deleted";
+    joinable_views_datasource DROP COLUMN IF EXISTS "deleted";
 
 ALTER TABLE
     joinable_views_datasource
-ADD
+ADD 
     CONSTRAINT fk_datasource_id FOREIGN KEY (datasource_id) REFERENCES datasource_bigquery (id) ON DELETE CASCADE;
 
 ALTER TABLE
     datasource_bigquery
-ADD
+ADD 
     CONSTRAINT fk_bigquery_dataset FOREIGN KEY (dataset_id) REFERENCES datasets (id) ON DELETE CASCADE;
