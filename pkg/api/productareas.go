d6c8ae91dd6a8feb1b6adc69e54fba97c82964e7
meng.zhu@nav.no <meng.zhu@nav.no>
2024-02-16 10:05:54 +0100

    use object to wrap productareas

--- pkg/api/productareas.go
+++ pkg/api/productareas.go
@@ -15,18 +15,22 @@ type TeamDto struct {
 }
 
 type ProductAreaDto struct {
-	teamkatalogen.ProductArea
-	Teams        []*TeamDto `json:"teams"`
-	DashboardURL string     `json:"dashboardURL"`
+	ProductAreas []ProductArea `json:"productAreas"`
 }
 
-func GetProductAreas(ctx context.Context) ([]*ProductAreaDto, *APIError) {
+type ProductArea struct {
+	teamkatalogen.ProductArea
+	Teams        []TeamDto `json:"teams"`
+	DashboardURL string    `json:"dashboardURL"`
+}
+
+func GetProductAreas(ctx context.Context) (*ProductAreaDto, *APIError) {
 	pa, err := tkClient.GetProductAreas(ctx)
 	if err != nil {
 		return nil, NewInternalError(err, "Failed to get product areas from Team Katalogen")
 	}
 
-	productAreas := make([]*ProductAreaDto, 0)
+	productAreas := make([]ProductArea, 0)
 	for _, p := range pa {
 		dash, err := querier.GetDashboard(ctx, p.ID)
 		if err != nil && err != sql.ErrNoRows {
@@ -37,7 +41,7 @@ func GetProductAreas(ctx context.Context) ([]*ProductAreaDto, *APIError) {
 		if err != nil {
 			return nil, NewInternalError(err, "Failed to get teams in product area from Team Katalogen")
 		}
-		teams := make([]*TeamDto, 0)
+		teams := make([]TeamDto, 0)
 		for _, tkTeam := range tkTeams {
 			dataproductsNumber, err := querier.GetDataproductsNumberByTeam(ctx, ptrToNullString(&tkTeam.ID))
 			if err != nil {
@@ -53,18 +57,20 @@ func GetProductAreas(ctx context.Context) ([]*ProductAreaDto, *APIError) {
 			if err != nil {
 				return nil, DBErrorToAPIError(err, "GetProductAreas(): failed to get insight products number")
 			}
-			teams = append(teams, &TeamDto{
+			teams = append(teams, TeamDto{
 				Team:                  *tkTeam,
 				DataproductsNumber:    int(dataproductsNumber),
 				StoriesNumber:         int(storiesNumber),
 				InsightProductsNumber: int(insightProductsNumber),
 			})
 		}
-		productAreas = append(productAreas, &ProductAreaDto{
+		productAreas = append(productAreas, ProductArea{
 			ProductArea:  *p,
 			Teams:        teams,
 			DashboardURL: dash.Url,
 		})
 	}
-	return productAreas, nil
+	return &ProductAreaDto{
+		ProductAreas: productAreas,
+	}, nil
 }
