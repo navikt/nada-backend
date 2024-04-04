4c6965b84b590f1173539c45ba33e772c84804a3
meng.zhu@nav.no <meng.zhu@nav.no>
2024-04-04 09:30:48 +0200

    update migration script for removing datastream

---
+++ pkg/database/migrations/0084_remove_datastream.sql
@@ -0,0 +1,53 @@
+-- +goose Up
+-- +goose StatementBegin
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            PERFORM pg_drop_replication_slot('ds_replication');
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
+-- +goose StatementEnd
+-- +goose StatementBegin
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            ALTER USER "datastream" WITH NOREPLICATION;
+            REVOKE SELECT ON ALL TABLES IN SCHEMA PUBLIC FROM "datastream";
+            ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC REVOKE SELECT ON TABLES FROM "datastream";
+            
+            DROP PUBLICATION "ds_publication";
+            ALTER USER "nada-backend" WITH NOREPLICATION;
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT('ds_replication', 'pgoutput');
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
+-- +goose StatementEnd
+-- +goose StatementBegin
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'datastream') THEN
+            ALTER USER "nada-backend" WITH REPLICATION;
+            CREATE PUBLICATION "ds_publication" FOR ALL TABLES;
+            
+            ALTER DEFAULT PRIVILEGES IN SCHEMA PUBLIC GRANT SELECT ON TABLES TO "datastream";
+            GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "datastream";
+            ALTER USER "datastream" WITH REPLICATION;
+        END IF;
+    END
+$$ LANGUAGE 'plpgsql';
+-- +goose StatementEnd
