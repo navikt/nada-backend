98106ddbaf448b24a6a965d0b8997f2f6c8f9d0f
Paul B. Beskow <paul@beskow.no>
2024-06-06 13:15:31 +0200

    feat(config): replicate existing structure in v2

---
+++ pkg/config/v2/config.go
@@ -0,0 +1,423 @@
+package config
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+
+	"github.com/mitchellh/mapstructure"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	defaultExtension = "yaml"
+	defaultTagName   = "yaml"
+)
+
+type Binder interface {
+	Bind(v *viper.Viper) error
+}
+
+type Loader interface {
+	Load(name, path, envPrefix string, binder Binder) (Config, error)
+}
+
+type Config struct {
+	Oauth              Oauth              `yaml:"oauth"`
+	Metabase           Metabase           `yaml:"metabase"`
+	Slack              Slack              `yaml:"slack"`
+	Server             Server             `yaml:"server"`
+	Postgres           Postgres           `yaml:"postgres"`
+	TeamsCatalogue     TeamsCatalogue     `yaml:"teams_catalogue"`
+	TreatmentCatalogue TreatmentCatalogue `yaml:"treatment_catalogue"`
+	GoogleGroups       GoogleGroups       `yaml:"google_groups"`
+	GCP                GCP                `yaml:"gcp"`
+	Cookies            Cookies            `yaml:"cookies"`
+	NaisConsole        NaisConsole        `yaml:"nais_console"`
+	API                API                `yaml:"api"`
+
+	AmplitudeAPIKey  string `yaml:"amplitude_api_key"`
+	LogLevel         string `yaml:"log_level"`
+	MockAuth         bool   `yaml:"mock_auth"`
+	SkipMetadataSync bool   `yaml:"skip_metadata_sync"`
+	Debug            bool   `yaml:"debug"`
+}
+
+func (c Config) Validate() error {
+	return validation.ValidateStruct(&c,
+		validation.Field(&c.Oauth, validation.Required),
+		validation.Field(&c.Metabase, validation.Required),
+		validation.Field(&c.Slack, validation.Required),
+		validation.Field(&c.Server, validation.Required),
+		validation.Field(&c.Postgres, validation.Required),
+		validation.Field(&c.TeamsCatalogue, validation.Required),
+		validation.Field(&c.TreatmentCatalogue, validation.Required),
+		validation.Field(&c.GoogleGroups, validation.Required),
+		validation.Field(&c.GCP, validation.Required),
+		validation.Field(&c.Cookies, validation.Required),
+		validation.Field(&c.NaisConsole, validation.Required),
+		validation.Field(&c.API, validation.Required),
+		validation.Field(&c.AmplitudeAPIKey, validation.Required),
+		validation.Field(&c.LogLevel, validation.Required),
+		validation.Field(&c.MockAuth, validation.Required),
+		validation.Field(&c.SkipMetadataSync, validation.Required),
+		validation.Field(&c.Debug, validation.Required),
+	)
+}
+
+type TreatmentCatalogue struct {
+	APIURL string `yaml:"api_url"`
+}
+
+func (t TreatmentCatalogue) Validate() error {
+	return validation.ValidateStruct(&t,
+		validation.Field(&t.APIURL, validation.Required, is.URL),
+	)
+}
+
+type API struct {
+	AuthToken string `yaml:"auth_token"`
+}
+
+func (a API) Validate() error {
+	return validation.ValidateStruct(&a,
+		validation.Field(&a.AuthToken, validation.Required),
+	)
+}
+
+type NaisConsole struct {
+	APIKey string `yaml:"api_key"`
+	APIURL string `yaml:"api_url"`
+}
+
+func (c NaisConsole) Validate() error {
+	return validation.ValidateStruct(&c,
+		validation.Field(&c.APIKey, validation.Required),
+		validation.Field(&c.APIURL, validation.Required, is.URL),
+	)
+}
+
+type GoogleGroups struct {
+	ImpersonationSubject string `yaml:"impersonation_subject"`
+	CredentialFile       string `yaml:"credential_file"`
+}
+
+func (g GoogleGroups) Validate() error {
+	return validation.ValidateStruct(&g,
+		validation.Field(&g.ImpersonationSubject, validation.Required),
+		validation.Field(&g.CredentialFile, validation.Required),
+	)
+}
+
+type TeamsCatalogue struct {
+	APIURL string `yaml:"api_url"`
+}
+
+func (t TeamsCatalogue) Validate() error {
+	return validation.ValidateStruct(&t,
+		validation.Field(&t.APIURL, validation.Required, is.URL),
+	)
+}
+
+type Slack struct {
+	Token      string `yaml:"token"`
+	WebhookURL string `yaml:"webhook_url"`
+}
+
+func (s Slack) Validate() error {
+	return validation.ValidateStruct(&s,
+		validation.Field(&s.Token, validation.Required),
+		validation.Field(&s.WebhookURL, validation.Required, is.URL),
+	)
+}
+
+type Metabase struct {
+	Username        string `yaml:"username"`
+	Password        string `yaml:"password"`
+	APIURL          string `yaml:"api_url"`
+	CredentialsPath string `yaml:"credentials_path"`
+}
+
+func (m Metabase) Validate() error {
+	return validation.ValidateStruct(&m,
+		validation.Field(&m.Username, validation.Required),
+		validation.Field(&m.Password, validation.Required),
+		validation.Field(&m.APIURL, validation.Required, is.URL),
+	)
+}
+
+type Github struct {
+	Organization        string `yaml:"organization"`
+	ApplicationID       int64  `yaml:"application_id"`
+	InstallationID      int64  `yaml:"installation_id"`
+	PrivateKeyPath      string `yaml:"private_key_path"`
+	RefreshIntervalMins int    `yaml:"refresh_interval_mins"`
+}
+
+func (g Github) Validate() error {
+	return validation.ValidateStruct(&g,
+		validation.Field(&g.Organization, validation.Required),
+		validation.Field(&g.ApplicationID, validation.Required),
+		validation.Field(&g.InstallationID, validation.Required),
+		validation.Field(&g.PrivateKeyPath, validation.Required),
+		validation.Field(&g.RefreshIntervalMins, validation.Required),
+	)
+}
+
+type Postgres struct {
+	UserName      string                `yaml:"user_name"`
+	Password      string                `yaml:"password"`
+	Host          string                `yaml:"host"`
+	Port          string                `yaml:"port"`
+	DatabaseName  string                `yaml:"database_name"`
+	SSLMode       string                `yaml:"ssl_mode"`
+	Configuration PostgresConfiguration `yaml:"configuration"`
+}
+
+func (p Postgres) Validate() error {
+	return validation.ValidateStruct(&p,
+		validation.Field(&p.UserName, validation.Required),
+		validation.Field(&p.Password, validation.Required),
+		validation.Field(&p.Host, validation.Required, is.Host),
+		validation.Field(&p.Port, validation.Required, is.Port),
+		validation.Field(&p.DatabaseName, validation.Required),
+		validation.Field(&p.SSLMode, validation.Required, validation.In("disable", "allow", "prefer", "require")),
+	)
+}
+
+func (p Postgres) ConnectionString() string {
+	return fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s",
+		p.UserName,
+		p.Password,
+		net.JoinHostPort(p.Host, p.Port),
+		p.DatabaseName,
+		p.SSLMode,
+	)
+}
+
+type PostgresConfiguration struct {
+	MaxIdleConnections int `yaml:"max_idle_connections"`
+	MaxOpenConnections int `yaml:"max_open_connections"`
+}
+
+type Server struct {
+	Hostname string `yaml:"hostname"`
+	Address  string `yaml:"address"`
+	Port     string `yaml:"port"`
+}
+
+func (s Server) Validate() error {
+	return validation.ValidateStruct(&s,
+		validation.Field(&s.Address, validation.Required, is.IP),
+		validation.Field(&s.Hostname, validation.Required, is.Host),
+		validation.Field(&s.Port, validation.Required, is.Port),
+	)
+}
+
+type Oauth struct {
+	ClientID     string `yaml:"client_id"`
+	ClientSecret string `yaml:"client_secret"`
+	TenantID     string `yaml:"tenant_id"`
+	RedirectURL  string `yaml:"redirect_url"`
+}
+
+func (o Oauth) Validate() error {
+	return validation.ValidateStruct(&o,
+		validation.Field(&o.ClientID, validation.Required),
+		validation.Field(&o.ClientSecret, validation.Required),
+		validation.Field(&o.TenantID, validation.Required),
+		validation.Field(&o.RedirectURL, validation.Required, is.URL),
+	)
+}
+
+type GCP struct {
+	Project  string   `yaml:"project"`
+	Region   string   `yaml:"region"`
+	GCS      GCS      `yaml:"gcs"`
+	BigQuery BigQuery `yaml:"big_query"`
+}
+
+func (g GCP) Validate() error {
+	return validation.ValidateStruct(&g,
+		validation.Field(&g.Project, validation.Required),
+		// Valid regions and zones:
+		// - https://cloud.google.com/compute/docs/regions-zones
+		validation.Field(&g.Region, validation.Required, validation.In("europe-north1")),
+		validation.Field(&g.GCS, validation.Required),
+		validation.Field(&g.BigQuery, validation.Required),
+	)
+}
+
+type GCS struct {
+	StoryBucketName string `yaml:"story_bucket_name"`
+}
+
+func (g GCS) Validate() error {
+	return validation.ValidateStruct(&g,
+		validation.Field(&g.StoryBucketName, validation.Required),
+	)
+}
+
+type BigQuery struct {
+	PseudoViewsDatasetName string `yaml:"pseudo_views_dataset_name"`
+}
+
+func (b BigQuery) Validate() error {
+	return validation.ValidateStruct(&b,
+		validation.Field(&b.PseudoViewsDatasetName, validation.Required),
+	)
+}
+
+type Cookies struct {
+	Redirect   CookieSettings `yaml:"redirect"`
+	OauthState CookieSettings `yaml:"oauth_state"`
+	Session    CookieSettings `yaml:"session"`
+}
+
+func (c Cookies) Validate() error {
+	return validation.ValidateStruct(&c,
+		validation.Field(&c.Redirect, validation.Required),
+		validation.Field(&c.OauthState, validation.Required),
+		validation.Field(&c.Session, validation.Required),
+	)
+}
+
+type CookieSettings struct {
+	Name     string `yaml:"name"`
+	MaxAge   int    `yaml:"max_age"`
+	Path     string `yaml:"path"`
+	Domain   string `yaml:"domain"`
+	SameSite string `yaml:"same_site"`
+	Secure   bool   `yaml:"secure"`
+	HttpOnly bool   `yaml:"http_only"`
+}
+
+func (c CookieSettings) GetSameSite() http.SameSite {
+	switch c.SameSite {
+	case "Strict":
+		return http.SameSiteStrictMode
+	case "Lax":
+		return http.SameSiteLaxMode
+	case "None":
+		return http.SameSiteNoneMode
+	default:
+		return http.SameSiteDefaultMode
+	}
+}
+
+func (c CookieSettings) Validate() error {
+	return validation.ValidateStruct(&c,
+		validation.Field(&c.Name, validation.Required),
+		validation.Field(&c.MaxAge, validation.Required),
+		validation.Field(&c.Path, validation.Required),
+		validation.Field(&c.Domain, validation.Required, is.Host),
+		// Valid SameSite values:
+		// - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#samesitesamesite-value
+		validation.Field(&c.SameSite, validation.Required, validation.In("Strict", "Lax", "None")),
+	)
+}
+
+type FileParts struct {
+	FileName string
+	Path     string
+}
+
+func ProcessConfigPath(configFile string) (FileParts, error) {
+	absolutePath, err := filepath.Abs(configFile)
+	if err != nil {
+		return FileParts{}, fmt.Errorf("convert to absolute path: %w", err)
+	}
+
+	// Extract file name and extension
+	fileName := filepath.Base(absolutePath)
+	path := filepath.Dir(absolutePath)
+	extension := filepath.Ext(fileName)
+
+	if strings.ReplaceAll(strings.ToLower(extension), ".", "") != defaultExtension {
+		return FileParts{}, fmt.Errorf("config file must have extension %s, got: %s", defaultExtension, extension)
+	}
+
+	return FileParts{
+		FileName: fileName[:len(fileName)-len(extension)],
+		Path:     path,
+	}, nil
+}
+
+func NewFileSystemLoader() *FileSystemLoader {
+	return &FileSystemLoader{}
+}
+
+type FileSystemLoader struct{}
+
+func (fs *FileSystemLoader) Load(name, path, envPrefix string, b Binder) (Config, error) {
+	v := viper.New()
+
+	v.AddConfigPath(path)
+	v.SetConfigName(name)
+	v.SetConfigType(defaultExtension)
+
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // So that env vars are translated properly
+	v.AutomaticEnv()
+
+	if b != nil {
+		err := b.Bind(v)
+		if err != nil {
+			return Config{}, err
+		}
+	}
+
+	v.SetEnvPrefix(envPrefix)
+
+	err := v.ReadInConfig()
+	if err != nil {
+		return Config{}, fmt.Errorf("read config: %w", err)
+	}
+
+	var config Config
+
+	err = v.Unmarshal(&config, func(cfg *mapstructure.DecoderConfig) {
+		cfg.TagName = defaultTagName // We use yaml tags in the config structs so we can marshal to yaml
+	})
+	if err != nil {
+		return Config{}, fmt.Errorf("unmarshal config: %w", err)
+	}
+
+	return config, nil
+}
+
+type EnvBinder struct {
+	binders map[string]string
+}
+
+func (e *EnvBinder) Bind(v *viper.Viper) error {
+	for envVar, key := range e.binders {
+		err := v.BindEnv(key, envVar)
+		if err != nil {
+			return fmt.Errorf("bind env var %s to key %s: %w", envVar, key, err)
+		}
+	}
+
+	return nil
+}
+
+func NewEnvBinder(binders map[string]string) *EnvBinder {
+	return &EnvBinder{
+		binders: binders,
+	}
+}
+
+func NewDefaultEnvBinder() *EnvBinder {
+	return NewEnvBinder(map[string]string{
+		"AZURE_APP_CLIENT_ID":                      "oauth.client_id",
+		"AZURE_APP_CLIENT_SECRET":                  "oauth.client_secret",
+		"AZURE_APP_TENANT_ID":                      "oauth.tenant_id",
+		"NAIS_DATABASE_NADA_BACKEND_NADA_PASSWORD": "postgres.password",
+	})
+}
---
+++ pkg/config/v2/config_test.go
@@ -0,0 +1,301 @@
+package config_test
+
+import (
+	"flag"
+	"github.com/navikt/nada-backend/pkg/config/v2"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"gopkg.in/yaml.v3"
+)
+
+var update = flag.Bool("update", false, "update golden files")
+
+func newFakeConfig() config.Config {
+	return config.Config{
+		Oauth: config.Oauth{
+			ClientID:     "fake_client_id",
+			ClientSecret: "fake_client_secret",
+			TenantID:     "fake_tenant_id",
+			RedirectURL:  "http://localhost:8080/auth/callback",
+		},
+		Metabase: config.Metabase{
+			Username:        "fake_username",
+			Password:        "fake_password",
+			APIURL:          "http://localhost:3000/api",
+			CredentialsPath: "/some/path",
+		},
+		Slack: config.Slack{
+			Token:      "fake_token",
+			WebhookURL: "http://localhost:8080/webhook",
+		},
+		Server: config.Server{
+			Hostname: "localhost",
+			Address:  "127.0.0.1",
+			Port:     "8080",
+		},
+		Postgres: config.Postgres{
+			UserName:     "fake_username",
+			Password:     "fake_password",
+			Host:         "http://localhost",
+			Port:         "5432",
+			DatabaseName: "something",
+			SSLMode:      "disabled",
+			Configuration: config.PostgresConfiguration{
+				MaxIdleConnections: 10,
+				MaxOpenConnections: 5,
+			},
+		},
+		TeamsCatalogue: config.TeamsCatalogue{
+			APIURL: "http://localhost:8080/api",
+		},
+		TreatmentCatalogue: config.TreatmentCatalogue{
+			APIURL: "http://localhost:8080/api",
+		},
+		GoogleGroups: config.GoogleGroups{
+			ImpersonationSubject: "something@example.com",
+			CredentialFile:       "/some/secret/path",
+		},
+		GCP: config.GCP{
+			Project: "fake_project",
+			Region:  "europe-north1",
+			GCS: config.GCS{
+				StoryBucketName: "fake_bucket",
+			},
+			BigQuery: config.BigQuery{
+				PseudoViewsDatasetName: "fake_dataset",
+			},
+		},
+		Cookies: config.Cookies{
+			Redirect: config.CookieSettings{
+				Name:     "redirect",
+				MaxAge:   0,
+				Path:     "some/path",
+				Domain:   "localhost",
+				SameSite: "lax",
+				Secure:   false,
+				HttpOnly: false,
+			},
+			OauthState: config.CookieSettings{
+
+				Name:     "auth",
+				MaxAge:   0,
+				Path:     "some/path",
+				Domain:   "localhost",
+				SameSite: "lax",
+				Secure:   false,
+				HttpOnly: false,
+			},
+			Session: config.CookieSettings{
+
+				Name:     "session",
+				MaxAge:   0,
+				Path:     "some/path",
+				Domain:   "localhost",
+				SameSite: "lax",
+				Secure:   false,
+				HttpOnly: false,
+			},
+		},
+		NaisConsole: config.NaisConsole{
+			APIKey: "fake_api_key",
+			APIURL: "http://localhost:8080/api",
+		},
+		API: config.API{
+			AuthToken: "fake_token",
+		},
+		AmplitudeAPIKey:  "fake_key",
+		LogLevel:         "info",
+		MockAuth:         false,
+		SkipMetadataSync: false,
+		Debug:            false,
+	}
+}
+
+func updateGoldenFiles(t *testing.T, filePath string, cfg config.Config) []byte {
+	t.Helper()
+
+	data, err := yaml.Marshal(cfg)
+	if err != nil {
+		t.Errorf("marshal config: %v", err)
+	}
+
+	err = os.WriteFile(filePath, data, 0o600)
+	if err != nil {
+		t.Errorf("write golden file: %v", err)
+	}
+
+	return data
+}
+
+func TestLoad(t *testing.T) {
+	if *update {
+		t.Log("Updating golden files")
+		updateGoldenFiles(t, "testdata/config.yaml", newFakeConfig())
+		t.Log("Done updating golden files")
+
+		return
+	}
+
+	testCases := []struct {
+		name      string
+		config    string
+		path      string
+		envPrefix string
+		loader    config.Loader
+		binder    config.Binder
+		envs      map[string]string
+		expect    config.Config
+		expectErr bool
+	}{
+		{
+			name:      "Standard config",
+			config:    "config",
+			path:      "testdata",
+			loader:    config.NewFileSystemLoader(),
+			expect:    newFakeConfig(),
+			expectErr: false,
+		},
+		{
+			name:   "Standard config with env overrides",
+			config: "config",
+			path:   "testdata",
+			loader: config.NewFileSystemLoader(),
+			expect: func() config.Config {
+				cfg := newFakeConfig()
+				cfg.Server.Address = "example.com"
+				cfg.GCP.Project = "project_x"
+
+				return cfg
+			}(),
+			envs: map[string]string{
+				"SERVER_ADDRESS": "example.com",
+				"GCP_PROJECT":    "project_x",
+			},
+		},
+		{
+			name:      "Standard config with env prefix overrides",
+			config:    "config",
+			path:      "testdata",
+			envPrefix: "nada",
+			loader:    config.NewFileSystemLoader(),
+			expect: func() config.Config {
+				cfg := newFakeConfig()
+				cfg.Server.Address = "example.com"
+				cfg.GCP.Project = "project_x"
+
+				return cfg
+			}(),
+			envs: map[string]string{
+				"NADA_SERVER_ADDRESS": "example.com",
+				"NADA_GCP_PROJECT":    "project_x",
+			},
+		},
+		{
+			name:      "Standard config with env overrides and binder",
+			config:    "config",
+			path:      "testdata",
+			envPrefix: "nada",
+			loader:    config.NewFileSystemLoader(),
+			binder: config.NewEnvBinder(map[string]string{
+				"SOME_RANDOM_SERVER_ADDRESS": "server.address",
+			}),
+			expect: func() config.Config {
+				cfg := newFakeConfig()
+				cfg.Server.Address = "this.example.com"
+
+				return cfg
+			}(),
+			envs: map[string]string{
+				"SOME_RANDOM_SERVER_ADDRESS": "this.example.com",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			for k, v := range tc.envs {
+				t.Setenv(k, v)
+			}
+
+			cfg, err := tc.loader.Load(tc.config, tc.path, tc.envPrefix, tc.binder)
+			if err != nil && !tc.expectErr {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if err == nil && tc.expectErr {
+				t.Errorf("expected error, got none")
+			}
+
+			if !tc.expectErr {
+				if diff := cmp.Diff(tc.expect, cfg); diff != "" {
+					t.Errorf("mismatch (-want +got):\n%s", diff)
+				}
+			}
+		})
+	}
+}
+
+func getWorkingDir(t *testing.T) string {
+	t.Helper()
+
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Errorf("get working dir: %v", err)
+	}
+
+	return wd
+}
+
+func TestProcessConfigPath(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name      string
+		path      string
+		expect    config.FileParts
+		expectErr bool
+	}{
+		{
+			name: "Valid config path",
+			path: "testdata/config.yaml",
+			expect: config.FileParts{
+				FileName: "config",
+				Path:     filepath.Join(getWorkingDir(t), "testdata"),
+			},
+		},
+		{
+			name:      "Invalid extension",
+			path:      "testdata/config.json",
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := config.ProcessConfigPath(tc.path)
+			if err != nil && !tc.expectErr {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if err == nil && tc.expectErr {
+				t.Errorf("expected error, got none")
+			}
+
+			if !tc.expectErr {
+				if diff := cmp.Diff(tc.expect, got); diff != "" {
+					t.Errorf("mismatch (-want +got):\n%s", diff)
+				}
+			}
+		})
+	}
+}
