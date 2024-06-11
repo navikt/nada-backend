f51930f02036737c949d2d5bd57566cc2eec5909
Paul B. Beskow <paul@beskow.no>
2024-06-11 13:41:33 +0200

    chore(service): add a serviceaccount service

---
+++ pkg/service/serviceaccount.go
@@ -0,0 +1,9 @@
+package service
+
+type ServiceAccountAPI interface {
+	DeleteServiceAccount(gcpProject, saEmail string) error
+	CreateServiceAccount(gcpProject string, ds *Dataset) ([]byte, string, error)
+}
+
+type ServiceAccountService interface {
+}
