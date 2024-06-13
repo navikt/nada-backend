57857dded494fdb847d106ef4ce250946af28692
Paul B. Beskow <paul@beskow.no>
2024-06-13 14:05:23 +0200

    chore(access): use services in ensurer

--- pkg/access_ensurer/ensurer.go
+++ pkg/access_ensurer/ensurer.go
@@ -6,57 +6,30 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/uuid"
 	"github.com/navikt/nada-backend/pkg/auth"
-	"github.com/navikt/nada-backend/pkg/bqclient"
-	"github.com/navikt/nada-backend/pkg/database/gensql"
 	"github.com/navikt/nada-backend/pkg/service"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
 )
 
-type Service interface {
-	GetUnrevokedExpiredAccess(ctx context.Context) ([]*service.Access, error)
-	GetBigqueryDatasource(ctx context.Context, dataproductID uuid.UUID, isReference bool) (*service.BigQuery, error)
-	RevokeAccessToDataset(ctx context.Context, id uuid.UUID, gcpProject string) error
-	GetPseudoDatasourcesToDelete(ctx context.Context) ([]*service.BigQuery, error)
-	SetDatasourceDeleted(ctx context.Context, id uuid.UUID) error
-	GetJoinableViewsWithReference(ctx context.Context) ([]gensql.GetJoinableViewsWithReferenceRow, error)
-	GetOwnerGroupOfDataset(ctx context.Context, datasetID uuid.UUID) (string, error)
-	ListActiveAccessToDataset(ctx context.Context, datasetID uuid.UUID) ([]*service.Access, error)
-	SetJoinableViewDeleted(ctx context.Context, joinableViewID uuid.UUID) error
-	GetJoinableViewsToBeDeletedWithRefDatasource(ctx context.Context) ([]gensql.GetJoinableViewsToBeDeletedWithRefDatasourceRow, error)
-}
-
 type Ensurer struct {
-	s                  Service
-	r                  Revoker
-	bq                 BigQuery
+	accessStorage       service.AccessStorage
+	accessService       service.AccessService
+	metabaseService     service.MetabaseService
+	dataProductsStorage service.DataProductsStorage
+	bigQueryStorage     service.BigQueryStorage
+	bigQueryAPI         service.BigQueryAPI
+	bigQueryService     service.BigQueryService
+	joinableViewService service.JoinableViewsService
+
 	googleGroups       *auth.GoogleGroupClient
 	centralDataProject string
 	log                *logrus.Entry
 	errs               *prometheus.CounterVec
 }
 
-type BigQuery interface {
-	DeleteJoinableDataset(ctx context.Context, datasetID string) error
-	DeleteJoinableView(ctx context.Context, joinableViewName, refProjectID, refDatasetID, refTableID string) error
-	DeletePseudoView(ctx context.Context, pseudoProjectID, pseudoDatasetID, pseudoTableID string) error
-}
-
-type Revoker interface {
-	Grant(ctx context.Context, projectID, dataset, table, member string) error
-	Revoke(ctx context.Context, projectID, dataset, table, member string) error
-}
-
-func NewEnsurer(s Service, r Revoker, bq BigQuery, googleGroups *auth.GoogleGroupClient, centralDataProject string, errs *prometheus.CounterVec, log *logrus.Entry) *Ensurer {
-	if s == nil {
-		s = ServiceWrapper{}
-	}
+func NewEnsurer(googleGroups *auth.GoogleGroupClient, centralDataProject string, errs *prometheus.CounterVec, log *logrus.Entry) *Ensurer {
 	return &Ensurer{
-		s:                  s,
-		r:                  r,
-		bq:                 bq,
 		googleGroups:       googleGroups,
 		centralDataProject: centralDataProject,
 		log:                log,
@@ -79,26 +52,33 @@ func (e *Ensurer) Run(ctx context.Context, frequency time.Duration) {
 }
 
 func (e *Ensurer) run(ctx context.Context) {
-	entries, err := e.s.GetUnrevokedExpiredAccess(ctx)
+	entries, err := e.accessStorage.GetUnrevokedExpiredAccess(ctx)
 	if err != nil {
 		e.log.WithError(err).Error("Getting unrevoked expired access entries from database")
 	}
 
 	for _, entry := range entries {
-		ds, err := e.s.GetBigqueryDatasource(ctx, entry.DatasetID, false)
+		ds, err := e.bigQueryStorage.GetBigqueryDatasource(ctx, entry.DatasetID, false)
 		if err != nil {
 			e.log.WithError(err).Error("Getting dataproduct datasource for expired access entry")
 			e.errs.WithLabelValues("GetBigqueryDatasource").Inc()
 			continue
 		}
-		if err := e.r.Revoke(ctx, ds.ProjectID, ds.Dataset, ds.Table, entry.Subject); err != nil {
+		if err := e.bigQueryAPI.Revoke(ctx, ds.ProjectID, ds.Dataset, ds.Table, entry.Subject); err != nil {
 			e.log.WithError(err).Errorf("Revoking IAM access for %v on %v.%v.%v", entry.Subject, ds.ProjectID, ds.Dataset, ds.Table)
 			e.errs.WithLabelValues("Revoke").Inc()
 			continue
 		}
-		if err := e.s.RevokeAccessToDataset(ctx, entry.ID, e.centralDataProject); err != nil {
+		// FIXME: need to call revoke access to dataset and metabse revoke access from access id
+		if err := e.accessService.RevokeAccessToDataset(ctx, entry.ID.String(), e.centralDataProject); err != nil {
 			e.log.WithError(err).Errorf("Setting access entry with ID %v to revoked in database", entry.ID)
 			e.errs.WithLabelValues("RevokeAccessToDataproduct").Inc()
+			continue
+		}
+
+		if err := e.metabaseService.RevokeMetabaseAccess(ctx, entry.DatasetID, entry.Subject); err != nil {
+			e.log.WithError(err).Errorf("Revoking access to Metabase for access ID %v", entry.ID)
+			e.errs.WithLabelValues("RevokeAccessToMetabase").Inc()
 			continue
 		}
 	}
@@ -122,7 +102,7 @@ func (e *Ensurer) run(ctx context.Context) {
 }
 
 func (e *Ensurer) ensureDeletePseudoViewBQForDeletedDataset(ctx context.Context) error {
-	pseudoDatasources, err := e.s.GetPseudoDatasourcesToDelete(ctx)
+	pseudoDatasources, err := e.bigQueryStorage.GetPseudoDatasourcesToDelete(ctx)
 	if err != nil {
 		return err
 	}
@@ -139,12 +119,12 @@ func (e *Ensurer) ensureDeletePseudoViewBQForDeletedDataset(ctx context.Context)
 			continue
 		}
 
-		if err := e.bq.DeletePseudoView(ctx, pds.ProjectID, pds.Dataset, pds.Table); err != nil {
+		if err := e.bigQueryAPI.DeletePseudoView(ctx, pds.ProjectID, pds.Dataset, pds.Table); err != nil {
 			e.log.WithError(err).Errorf("deleting pseudo view with deleted dataset %v", pds.Dataset)
 			continue
 		}
 
-		if err := e.s.SetDatasourceDeleted(ctx, pds.ID); err != nil {
+		if err := e.dataProductsStorage.SetDatasourceDeleted(ctx, pds.ID); err != nil {
 			e.log.WithError(err).Errorf("setting pseudo view deleted in db, view id: %v", pds.ID)
 		} else {
 			e.log.Infof("pseudo view without dataset deleted: %v", pds.ID)
@@ -154,13 +134,13 @@ func (e *Ensurer) ensureDeletePseudoViewBQForDeletedDataset(ctx context.Context)
 }
 
 func (e *Ensurer) ensureDeleteJoinableViewBQForDeletedDataset(ctx context.Context) error {
-	jvdatasources, err := e.s.GetJoinableViewsToBeDeletedWithRefDatasource(ctx)
+	jvdatasources, err := e.joinableViewService.GetJoinableViewsToBeDeletedWithRefDatasource(ctx)
 	if err != nil {
 		return err
 	}
 
 	for _, jvds := range jvdatasources {
-		err := e.bq.DeleteJoinableView(ctx, jvds.JoinableViewName, jvds.BqProjectID, jvds.BqDatasetID, jvds.BqTableID)
+		err := e.bigQueryAPI.DeleteJoinableView(ctx, jvds.JoinableViewName, jvds.BqProjectID, jvds.BqDatasetID, jvds.BqTableID)
 		if err != nil {
 			e.log.WithError(err).Errorf("deleting joinable view with deleted pseudo-datasource %v %v.%v.%v", jvds.JoinableViewName, jvds.BqProjectID, jvds.BqDatasetID, jvds.BqTableID)
 			continue
@@ -170,8 +150,14 @@ func (e *Ensurer) ensureDeleteJoinableViewBQForDeletedDataset(ctx context.Contex
 	return nil
 }
 
+// FIXME: duplicated
+func makeJoinableViewName(projectID, datasetID, tableID string) string {
+	// datasetID will always be same markedsplassen dataset id
+	return fmt.Sprintf("%v_%v", projectID, tableID)
+}
+
 func (e *Ensurer) ensureJoinableViewAccesses(ctx context.Context) error {
-	joinableViews, err := e.s.GetJoinableViewsWithReference(ctx)
+	joinableViews, err := e.joinableViewService.GetJoinableViewsWithReference(ctx)
 	if err != nil {
 		e.log.WithError(err).Error("getting joinable views with reference")
 		return err
@@ -180,12 +166,12 @@ func (e *Ensurer) ensureJoinableViewAccesses(ctx context.Context) error {
 OUTER:
 	for _, jv := range joinableViews {
 		if hasExpired(jv) {
-			if err := e.bq.DeleteJoinableDataset(ctx, jv.JoinableViewDataset); err != nil {
+			if err := e.bigQueryAPI.DeleteJoinableDataset(ctx, jv.JoinableViewDataset); err != nil {
 				e.log.WithError(err).Errorf("deleting expired joinable view dataset %v", jv.JoinableViewDataset)
 				e.errs.WithLabelValues("DeleteExpiredDataset").Inc()
 				continue
 			}
-			if err := e.s.SetJoinableViewDeleted(ctx, jv.JoinableViewID); err != nil {
+			if err := e.joinableViewService.SetJoinableViewDeleted(ctx, jv.JoinableViewID); err != nil {
 				e.log.WithError(err).Errorf("setting joinable view deleted in db, view id: %v", jv.JoinableViewID)
 				e.errs.WithLabelValues("SetJoinableViewDeleted").Inc()
 				continue
@@ -193,8 +179,8 @@ OUTER:
 			continue
 		}
 
-		joinableViewName := bqclient.MakeJoinableViewName(jv.PseudoProjectID, jv.PseudoDataset, jv.PseudoTable)
-		datasetOwnerGroup, err := e.s.GetOwnerGroupOfDataset(ctx, jv.PseudoViewID)
+		joinableViewName := makeJoinableViewName(jv.PseudoProjectID, jv.PseudoDataset, jv.PseudoTable)
+		datasetOwnerGroup, err := e.dataProductsStorage.GetOwnerGroupOfDataset(ctx, jv.PseudoViewID)
 		if err != nil {
 			e.log.WithError(err).Errorf("getting owner group of dataset: %v", jv.PseudoViewID)
 			return err
@@ -206,7 +192,7 @@ OUTER:
 
 		for _, userGroup := range userGroups {
 			if userGroup.Email == datasetOwnerGroup {
-				if err := e.r.Grant(ctx, e.centralDataProject, jv.JoinableViewDataset, joinableViewName, fmt.Sprintf("user:%v", jv.Owner)); err != nil {
+				if err := e.bigQueryAPI.Grant(ctx, e.centralDataProject, jv.JoinableViewDataset, joinableViewName, fmt.Sprintf("user:%v", jv.Owner)); err != nil {
 					e.log.WithError(err).Errorf("Granting IAM access for %v on %v.%v.%v", jv.Owner, e.centralDataProject, jv.JoinableViewDataset, joinableViewName)
 					e.errs.WithLabelValues("Grant").Inc()
 					continue
@@ -215,7 +201,7 @@ OUTER:
 			}
 		}
 
-		accesses, err := e.s.ListActiveAccessToDataset(ctx, jv.PseudoViewID)
+		accesses, err := e.accessStorage.ListActiveAccessToDataset(ctx, jv.PseudoViewID)
 		if err != nil {
 			e.log.WithError(err).Errorf("listing active access to dataset: %v", jv.PseudoViewID)
 			return err
@@ -229,7 +215,7 @@ OUTER:
 			}
 			subjectWithoutType := subjectParts[1]
 			if subjectWithoutType == jv.Owner {
-				if err := e.r.Grant(ctx, e.centralDataProject, jv.JoinableViewDataset, joinableViewName, fmt.Sprintf("user:%v", jv.Owner)); err != nil {
+				if err := e.bigQueryAPI.Grant(ctx, e.centralDataProject, jv.JoinableViewDataset, joinableViewName, fmt.Sprintf("user:%v", jv.Owner)); err != nil {
 					e.log.WithError(err).Errorf("Granting IAM access for %v on %v.%v.%v", jv.Owner, e.centralDataProject, jv.JoinableViewDataset, joinableViewName)
 					e.errs.WithLabelValues("Grant").Inc()
 					continue
@@ -238,7 +224,7 @@ OUTER:
 			}
 		}
 
-		if err := e.r.Revoke(ctx, e.centralDataProject, jv.JoinableViewDataset, joinableViewName, fmt.Sprintf("user:%v", jv.Owner)); err != nil {
+		if err := e.bigQueryAPI.Revoke(ctx, e.centralDataProject, jv.JoinableViewDataset, joinableViewName, fmt.Sprintf("user:%v", jv.Owner)); err != nil {
 			e.log.WithError(err).Errorf("Revoking IAM access for %v on %v.%v.%v", jv.Owner, e.centralDataProject, jv.JoinableViewDataset, joinableViewName)
 			e.errs.WithLabelValues("Revoke").Inc()
 			continue
@@ -248,7 +234,7 @@ OUTER:
 	return nil
 }
 
-func hasExpired(jv gensql.GetJoinableViewsWithReferenceRow) bool {
+func hasExpired(jv service.JoinableViewWithReference) bool {
 	if jv.Expires.Valid {
 		return jv.Expires.Time.Before(time.Now())
 	}
