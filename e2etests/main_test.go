f8b815139dd123d38cfa6be25ff176f372dc51dd
Erik Vattekar <erik.vattekar@nav.no>
2024-08-06 12:56:49 +0200

    remove e2e tests

---
+++ e2etests/main_test.go
@@ -0,0 +1,186 @@
+//go:build integration_test
+
+package e2etests
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strconv"
+	"testing"
+
+	graphProm "github.com/99designs/gqlgen-contrib/prometheus"
+	"github.com/navikt/nada-backend/pkg/access"
+	"github.com/navikt/nada-backend/pkg/amplitude"
+	"github.com/navikt/nada-backend/pkg/api"
+	"github.com/navikt/nada-backend/pkg/auth"
+	"github.com/navikt/nada-backend/pkg/bqclient"
+	"github.com/navikt/nada-backend/pkg/database"
+	"github.com/navikt/nada-backend/pkg/database/gensql"
+	"github.com/navikt/nada-backend/pkg/event"
+	"github.com/navikt/nada-backend/pkg/gcs"
+	"github.com/navikt/nada-backend/pkg/polly"
+	"github.com/navikt/nada-backend/pkg/slack"
+	"github.com/navikt/nada-backend/pkg/teamkatalogen"
+	"github.com/navikt/nada-backend/pkg/teamprojectsupdater"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	repo   *database.Repo
+	server *httptest.Server
+)
+
+func TestMain(m *testing.M) {
+	dockerHost := os.Getenv("HOME") + "/.colima/docker.sock"
+	_, err := os.Stat(dockerHost)
+	if err != nil {
+		// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+		dockerHost = ""
+	} else {
+		dockerHost = "unix://" + dockerHost
+	}
+
+	pool, err := dockertest.NewPool(dockerHost)
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.Run("postgres", "12", []string{"POSTGRES_PASSWORD=postgres", "POSTGRES_DB=nada"})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+	resource.Expire(120) // setting resource timeout as postgres container is not terminated automatically
+
+	port, err := findAvailableHostPort()
+	if err != nil {
+		log.Fatalf("Could not start gcs resource: %s", err)
+	}
+
+	gcsResource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "fsouza/fake-gcs-server",
+		Tag:        "1.44",
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"4443/tcp": {{HostIP: "localhost", HostPort: strconv.Itoa(port) + "/tcp"}},
+		},
+		Entrypoint: []string{"/bin/fake-gcs-server", "-scheme", "http", "-public-host", "localhost:" + strconv.Itoa(port)},
+	}, func(config *docker.HostConfig) {
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{
+			Name: "no",
+		}
+	})
+	if err != nil {
+		log.Fatalf("could not start gcs resource %v", err)
+	}
+
+	os.Setenv("STORAGE_EMULATOR_HOST", fmt.Sprintf("http://localhost:%v/storage/v1/", gcsResource.GetPort("4443/tcp")))
+	defer os.Unsetenv("STORAGE_EMULATOR_HOST")
+
+	var dbString string
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err := pool.Retry(func() error {
+		var err error
+		dbString = "user=postgres dbname=nada sslmode=disable password=postgres host=localhost port=" + resource.GetPort("5432/tcp")
+		db, err := sql.Open("postgres", dbString)
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	gcsClient, err := gcs.New(context.Background(), storyBucket, logrus.NewEntry(logrus.StandardLogger()))
+	if err != nil {
+		panic(err)
+	}
+
+	repo, err = database.New(dbString, 2, 0, &event.Manager{}, logrus.NewEntry(logrus.StandardLogger()), "nav-central-data-dev-e170")
+	if err != nil {
+		panic(err)
+	}
+
+	tpu, err := teamprojectsupdater.NewMockTeamProjectsUpdater(context.Background(), repo)
+	if err != nil {
+		panic(err)
+	}
+
+	promReg := prometheus.NewRegistry()
+	graphProm.RegisterOn(promReg)
+
+	gqlServer := graph.New(
+		repo,
+		bqclient.NewMock(),
+		tpu.TeamProjectsMapping,
+		access.NewNoop(),
+		teamkatalogen.NewMock(),
+		slack.NewMockSlackClient(logrus.StandardLogger()),
+		polly.NewMock("https://some.url"),
+		"",
+		logrus.StandardLogger().WithField("subsystem", "graphql"),
+	)
+	srv := api.New(
+		repo,
+		gcsClient,
+		teamkatalogen.NewMock(),
+		&mockAuthHandler{},
+		auth.MockJWTValidatorMiddleware(),
+		gqlServer,
+		prometheus.NewRegistry(),
+		amplitude.NewMock(),
+		"",
+		logrus.StandardLogger(),
+	)
+
+	server = httptest.NewServer(srv)
+
+	code := m.Run()
+
+	// You can't defer this because os.Exit doesn't care for defer
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	if err := pool.Purge(gcsResource); err != nil {
+		log.Fatalf("Could not purge gcs resource: %s", err)
+	}
+
+	os.Exit(code)
+}
+
+func findAvailableHostPort() (int, error) {
+	if a, err := net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
+		var l *net.TCPListener
+		if l, err = net.ListenTCP("tcp", a); err == nil {
+			defer l.Close()
+			return l.Addr().(*net.TCPAddr).Port, nil
+		}
+	}
+	return 0, fmt.Errorf("could not find a port")
+}
+
+func stringToPtr(s string) *string {
+	return &s
+}
+
+type noopDatasetEnricher struct{}
+
+func (n noopDatasetEnricher) UpdateSchema(ctx context.Context, ds gensql.DatasourceBigquery) error {
+	return nil
+}
+
+type mockAuthHandler struct{}
+
+func (m *mockAuthHandler) Login(w http.ResponseWriter, r *http.Request)    {}
+func (m *mockAuthHandler) Callback(w http.ResponseWriter, r *http.Request) {}
+func (m *mockAuthHandler) Logout(w http.ResponseWriter, r *http.Request)   {}
