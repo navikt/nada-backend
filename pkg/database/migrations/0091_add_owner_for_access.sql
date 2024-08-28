60d9aab935db56fbc4994eaf100201ec03048a95
Erik Vattekar <erik.vattekar@nav.no>
2024-08-28 15:41:32 +0200

    no need for the type prefix for the access owner

--- pkg/database/migrations/0091_add_owner_for_access.sql
+++ pkg/database/migrations/0091_add_owner_for_access.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 ALTER TABLE dataset_access ADD COLUMN "owner" TEXT;
 
-UPDATE dataset_access SET "owner" = existing_dataset_access.subject
+UPDATE dataset_access SET "owner" = SPLIT_PART(existing_dataset_access.subject, ':', 2)
 FROM (SELECT "id", "subject" FROM dataset_access) AS existing_dataset_access
 WHERE dataset_access.id = existing_dataset_access.id;
 
