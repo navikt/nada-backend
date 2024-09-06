ef7a945c390d92b9ce5d1c6944e90447fbb0669d
Erik Vattekar <erik.vattekar@nav.no>
2024-09-06 11:31:39 +0200

    fix(metabase): need to grant all users access to allusers group in metabase after adding open database

--- pkg/service/core/service_metabase.go
+++ pkg/service/core/service_metabase.go
@@ -436,8 +436,11 @@ func (s *metabaseService) addAllUsersDataset(ctx context.Context, dsID uuid.UUID
 		if err != nil {
 			return errs.E(op, err)
 		}
+	}
 
-		return nil
+	meta, err = s.metabaseStorage.GetMetadata(ctx, dsID, false)
+	if err != nil {
+		return errs.E(op, err)
 	}
 
 	// All users database already exists in metabase
