119e06b077e167ad70399ee245e1ad84f33f2756
Erik Vattekar <erik.vattekar@nav.no>
2024-09-06 12:25:45 +0200

    Revert "fix(metabase): do not attempt to delete all users group on adding open database"
    This reverts commit c622b2441dcfceef63716b1e321b33a0401271f4.

--- pkg/service/core/service_metabase.go
+++ pkg/service/core/service_metabase.go
@@ -454,7 +454,7 @@ func (s *metabaseService) addAllUsersDataset(ctx context.Context, dsID uuid.UUID
 		return errs.E(op, err)
 	}
 
-	if meta.PermissionGroupID != nil && *meta.PermissionGroupID == 0 {
+	if meta.PermissionGroupID != nil {
 		err = s.metabaseAPI.DeletePermissionGroup(ctx, *meta.PermissionGroupID)
 		if err != nil {
 			return errs.E(op, err)
